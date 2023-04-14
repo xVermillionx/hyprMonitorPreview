bce1faf5316f04b7f648c6b8ec0066a321d75a3f
xVermillionx <xVermillionx@notvalid>
2023-04-15 00:10:01 +0200

    Put Functions in previewMonitorConfig into helpers.h TMP!

---
+++ src/helpers.h
@@ -0,0 +1,229 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+#include <ncurses.h>
+#include <cmath>
+#include <signal.h>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <thread>
+#include <chrono>
+#include "hyprglobal.h"
+
+float maxfactor;
+
+int row = LINES;
+int col = COLS;
+
+cchar_t tr,tl,br,bl,s,h;
+
+typedef struct WINDOW2 {
+  struct monitor mon;
+  WINDOW* win;
+} WINDOW2;
+
+void start_curses(mmask_t old){
+  initscr();       // Initialize ncurses screen
+  cbreak();
+  noecho();        // Don't display user input
+  keypad(stdscr, TRUE);
+  // Enable mouse support and track all mouse events
+  // mousemask(BUTTON1_CLICKED | BUTTON2_CLICKED | BUTTON3_CLICKED | BUTTON4_PRESSED | BUTTON5_PRESSED | REPORT_MOUSE_POSITION | ALL_MOUSE_EVENTS, &old);
+  mousemask(BUTTON1_PRESSED | BUTTON1_RELEASED | BUTTON3_CLICKED |  REPORT_MOUSE_POSITION | ALL_MOUSE_EVENTS, &old);
+  // Set the mouse click interval to 1000 milliseconds (1 second)
+  mouseinterval(80);
+  curs_set(0);     // Hide cursor
+
+  setlocale(LC_ALL, "");
+
+  setcchar(&tl, L"╭", WA_NORMAL, 0, NULL);
+  setcchar(&bl, L"╰", WA_NORMAL, 0, NULL);
+  setcchar(&tr, L"╮", WA_NORMAL, 0, NULL);
+  setcchar(&br, L"╯", WA_NORMAL, 0, NULL);
+  setcchar(&s,  L"│", WA_NORMAL, 0, NULL);
+  setcchar(&h,  L"─", WA_NORMAL, 0, NULL);
+
+  if(!has_colors() || !can_change_color()){
+    printw("Terminal doesnt support colors or cannot change them!");
+    exit(-1);
+  }
+  start_color();
+  use_default_colors(); //Set colors to normal
+  init_pair(1, COLOR_WHITE, -1);
+  init_pair(2, COLOR_BLACK, -1);
+  init_pair(3, COLOR_YELLOW, -1);
+  init_pair(4, COLOR_BLUE, -1);
+  use_default_colors(); //Set colors to normal
+
+  signal(SIGWINCH, [](int __attribute__((unused)) sig)->void {
+    endwin(); // End curses mode
+
+    refresh(); // Clear the screen
+    // Handle the resize event here
+    // ...
+    refresh(); // Redraw the screen
+    // Re-initialize ncurses
+    initscr();
+    // Turn off keyboard echoing and enable special key input
+    noecho();
+    keypad(stdscr, TRUE);
+  });
+}
+
+enum rotation& operator++(enum rotation& r) {
+    r = static_cast<enum rotation>((static_cast<int>(r) + 1) % (int)FLIPPED);
+    return r;
+}
+enum rotation& operator--(enum rotation& r) {
+    r = static_cast<enum rotation>((static_cast<int>(r) - 1) % (int)FLIPPED);
+    return r;
+}
+
+void updateWindow(WINDOW* win, struct monitor m) {
+  int height, width;
+
+  float hfactor = maxfactor;
+  float wfactor = hfactor/2;
+
+  if(m.transform%2){
+    height = floor(row * (m.pos.width /wfactor/m.scale));
+    width  = floor(col * (m.pos.height/hfactor/m.scale));
+  } else {
+    height = floor(row * (m.pos.height/wfactor/m.scale));
+    width  = floor(col * (m.pos.width /hfactor/m.scale));
+  }
+  int y    = floor(row * (m.res.y/wfactor));
+  int x    = floor(col * (m.res.x/hfactor));
+
+  // Order matters!
+  wresize(win, height, width);
+  mvwin(win, y, x);
+  wborder_set(win, (const cchar_t*)&s, (const cchar_t*)&s, (const cchar_t*)&h, (const cchar_t*)&h, (const cchar_t*)&tl, (const cchar_t*)&tr, (const cchar_t*)&bl, (const cchar_t*)&br);
+
+  int hcenter = height/2;
+  int wcenter = width/2;
+  mvwprintw(win, hcenter, wcenter-strlen(m.name)/2, m.name);
+  std::stringstream s;
+  s << x << ' ' << m.pos.width << 'x' << m.pos.height;
+  std::string dim = s.str();
+  mvwprintw(win, hcenter+1, wcenter-dim.length()/2, dim.c_str());
+}
+
+WINDOW2 createWindow(struct monitor m){
+    WINDOW *win = newwin(0, 0, 0, 0);
+    box_set(win, 0, 0); // Create a box around the window
+    updateWindow(win, m);
+    WINDOW2 ret{m, win};
+    return ret;
+}
+
+void chg_cchar_col(cchar_t &c, NCURSES_PAIRS_T col_pair){
+  c.ext_color = col_pair;
+}
+
+
+// set borders color 0 for normal
+void chg_border_col(NCURSES_PAIRS_T col_pair){
+  chg_cchar_col(tr, col_pair);
+  chg_cchar_col(tl, col_pair);
+  chg_cchar_col(br, col_pair);
+  chg_cchar_col(bl, col_pair);
+  chg_cchar_col(s,  col_pair);
+  chg_cchar_col(h,  col_pair);
+}
+// wcolor_set(windows.back().win,COLOR_PAIR(5),nullptr); ??
+
+bool updateJson(Json::Value& root){
+  root.clear();
+  Json::Reader reader;
+  bool parsingSuccessful = reader.parse(getMonitors(), root);
+  if (!parsingSuccessful)
+  {
+    std::cout << "Failed to parse"
+              << reader.getFormattedErrorMessages();
+  }
+  return parsingSuccessful;
+}
+
+struct monitor jsonMonitorToMonitor(const Json::Value& monitor) {
+  return {
+    .name = monitor["name"].asCString(),
+    .pos = {
+      .height = monitor["height"].asUInt(),
+      .width = monitor["width"].asUInt()
+    },
+    .res = {
+      .x = monitor["x"].asUInt(),
+      .y = monitor["y"].asUInt()
+    },
+    .hz = monitor["refreshRate"].asFloat(),
+    .scale = monitor["scale"].asFloat(),
+    .transform = (enum rotation)monitor["transform"].asUInt(),
+    .id = monitor["id"].asInt(),
+    .focused = monitor["focused"].asBool()
+  };
+}
+
+void createWindowsFromJson(std::vector<WINDOW2> &windows, const Json::Value &root) {
+  for (const Json::Value &monitor : root){
+    bool focused = monitor["focused"].asBool();
+    if(focused){
+      chg_border_col(4);
+    }
+    windows.push_back(createWindow(jsonMonitorToMonitor(monitor)));
+    if(focused){
+      wbkgd(windows.back().win, COLOR_PAIR(4));
+      chg_border_col(0);
+    }
+  }
+}
+
+void updateWindowsFromOwnMonitor(std::vector<WINDOW2> &windows){
+  for(auto& win2 : windows) {
+    updateWindow(win2.win, win2.mon);
+  }
+}
+
+void updateWindowsFromJson(std::vector<WINDOW2> &windows, const Json::Value &root) {
+  for (const auto &monitor : root){
+    bool focused = monitor["focused"].asBool();
+    if(focused){
+      chg_border_col(4);
+    }
+    for(auto& win2 : windows) {
+      if(!strcmp(win2.mon.name, monitor["name"].asCString())){
+        win2.mon = jsonMonitorToMonitor(monitor);
+        updateWindow(win2.win, win2.mon);
+      }
+    }
+    if(focused){
+      wbkgd(windows.back().win, COLOR_PAIR(4));
+      chg_border_col(0);
+    }
+  }
+}
+
+WINDOW2* getWindowAt(std::vector<WINDOW2> &windows, int x, int y){
+  WINDOW2* ret = nullptr;
+  for(auto& win2 : windows){
+    WINDOW* win = win2.win;
+    if(wenclose(win, y, x)) {
+      ret = &win2;
+      break;
+    }
+  }
+  return ret;
+}
+
+void renderWindows(std::vector<WINDOW2> &windows, std::chrono::milliseconds delay = 0ms) {
+  refresh();
+  for(const auto& win2 : windows) {
+    WINDOW* win = win2.win;
+    wrefresh(win);
+    if(delay > 0ms) std::this_thread::sleep_for(delay);
+  }
+  refresh();
+}
+
+#endif /* HELPER_H_H */
--- src/previewMonitorConfig.cpp
+++ src/previewMonitorConfig.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-// #include <json/>
 #include <ncurses.h>
 #include <signal.h>
 
@@ -11,7 +10,6 @@
 #include <chrono>
 using namespace std::chrono_literals;
 #include <sstream>
-// #include <algorithm>
 
 #include <json/json.h>
 #include <json/reader.h>
@@ -23,249 +21,32 @@ using namespace std::chrono_literals;
 #include "runUnixCMD.h"
 #include "hyprsocket.h"
 #include "hyprsocket2.h"
-
-enum rotation& operator++(enum rotation& r) {
-    r = static_cast<enum rotation>((static_cast<int>(r) + 1) % (int)FLIPPED);
-    return r;
-}
-enum rotation& operator--(enum rotation& r) {
-    r = static_cast<enum rotation>((static_cast<int>(r) - 1) % (int)FLIPPED);
-    return r;
-}
-
-
-cchar_t tr,tl,br,bl,s,h;
-
-float maxfactor;
-
-int row = LINES;
-int col = COLS;
-
-typedef struct WINDOW2 {
-  struct monitor mon;
-  WINDOW* win;
-} WINDOW2;
-
-void updateWindow(WINDOW* win, struct monitor m) {
-  int height, width;
-
-  float hfactor = maxfactor;
-  float wfactor = hfactor/2;
-
-  if(m.transform%2){
-    height = floor(row * (m.pos.width /wfactor/m.scale));
-    width  = floor(col * (m.pos.height/hfactor/m.scale));
-  } else {
-    height = floor(row * (m.pos.height/wfactor/m.scale));
-    width  = floor(col * (m.pos.width /hfactor/m.scale));
-  }
-  int y    = floor(row * (m.res.y/wfactor));
-  int x    = floor(col * (m.res.x/hfactor));
-
-  // Order matters!
-  wresize(win, height, width);
-  mvwin(win, y, x);
-  wborder_set(win, (const cchar_t*)&s, (const cchar_t*)&s, (const cchar_t*)&h, (const cchar_t*)&h, (const cchar_t*)&tl, (const cchar_t*)&tr, (const cchar_t*)&bl, (const cchar_t*)&br);
-
-  int hcenter = height/2;
-  int wcenter = width/2;
-  mvwprintw(win, hcenter, wcenter-strlen(m.name)/2, m.name);
-  std::stringstream s;
-  s << x << ' ' << m.pos.width << 'x' << m.pos.height;
-  std::string dim = s.str();
-  mvwprintw(win, hcenter+1, wcenter-dim.length()/2, dim.c_str());
-}
-
-WINDOW2 createWindow(struct monitor m){
-    WINDOW *win = newwin(0, 0, 0, 0);
-    box_set(win, 0, 0); // Create a box around the window
-    updateWindow(win, m);
-    WINDOW2 ret{m, win};
-    return ret;
-}
-
-void chg_cchar_col(cchar_t &c, NCURSES_PAIRS_T col_pair){
-  c.ext_color = col_pair;
-}
-
-
-// set borders color 0 for normal
-void chg_border_col(NCURSES_PAIRS_T col_pair){
-  chg_cchar_col(tr, col_pair);
-  chg_cchar_col(tl, col_pair);
-  chg_cchar_col(br, col_pair);
-  chg_cchar_col(bl, col_pair);
-  chg_cchar_col(s,  col_pair);
-  chg_cchar_col(h,  col_pair);
-}
-// wcolor_set(windows.back().win,COLOR_PAIR(5),nullptr); ??
+#include "helpers.h"
 
 std::mutex charLock;
 
-// void hyprSocketFocusMonitor(std::atomic<char[50]> &cur_mon, std::atomic<bool> &mutex) {
-// void hyprSocketFocusMonitor(char[50] &cur_mon, std::atomic<bool> &mutex) {
 void hyprSocketFocusMonitor(char* cur_mon, std::atomic<bool> &mutex) {
-// void hyprSocketFocusMonitor(std::shared_ptr<char[]> &cur_mon, std::atomic<bool> &mutex) {
   hs2_initSocketConnection();
   setDisplayRemote(cur_mon, mutex);
   hs2_closeSocketConnection();
 }
 
-bool updateJson(Json::Value& root){
-  root.clear();
-  Json::Reader reader;
-  bool parsingSuccessful = reader.parse(getMonitors(), root);
-  if (!parsingSuccessful)
-  {
-    std::cout << "Failed to parse"
-              << reader.getFormattedErrorMessages();
-  }
-  return parsingSuccessful;
-}
-
-struct monitor jsonMonitorToMonitor(const Json::Value& monitor) {
-  return {
-    .name = monitor["name"].asCString(),
-    .pos = {
-      .height = monitor["height"].asUInt(),
-      .width = monitor["width"].asUInt()
-    },
-    .res = {
-      .x = monitor["x"].asUInt(),
-      .y = monitor["y"].asUInt()
-    },
-    .hz = monitor["refreshRate"].asFloat(),
-    .scale = monitor["scale"].asFloat(),
-    .transform = (enum rotation)monitor["transform"].asUInt(),
-    .id = monitor["id"].asInt(),
-    .focused = monitor["focused"].asBool()
-  };
-}
-
-void createWindowsFromJson(std::vector<WINDOW2> &windows, const Json::Value &root) {
-  for (const Json::Value &monitor : root){
-    bool focused = monitor["focused"].asBool();
-    if(focused){
-      chg_border_col(4);
-    }
-    windows.push_back(createWindow(jsonMonitorToMonitor(monitor)));
-    if(focused){
-      wbkgd(windows.back().win, COLOR_PAIR(4));
-      chg_border_col(0);
-    }
-  }
-}
-
-void updateWindowsFromOwnMonitor(std::vector<WINDOW2> &windows){
-  for(auto& win2 : windows) {
-    updateWindow(win2.win, win2.mon);
-  }
-}
-
-void updateWindowsFromJson(std::vector<WINDOW2> &windows, const Json::Value &root) {
-  for (const auto &monitor : root){
-    bool focused = monitor["focused"].asBool();
-    if(focused){
-      chg_border_col(4);
-    }
-    for(auto& win2 : windows) {
-      if(!strcmp(win2.mon.name, monitor["name"].asCString())){
-        win2.mon = jsonMonitorToMonitor(monitor);
-        updateWindow(win2.win, win2.mon);
-      }
-    }
-    if(focused){
-      wbkgd(windows.back().win, COLOR_PAIR(4));
-      chg_border_col(0);
-    }
-  }
-}
-
-WINDOW2* getWindowAt(std::vector<WINDOW2> &windows, int x, int y){
-  WINDOW2* ret = nullptr;
-  for(auto& win2 : windows){
-    WINDOW* win = win2.win;
-    if(wenclose(win, y, x)) {
-      ret = &win2;
-      break;
-    }
-  }
-  return ret;
-}
-
-void renderWindows(std::vector<WINDOW2> &windows, std::chrono::milliseconds delay = 0ms) {
-  refresh();
-  for(const auto& win2 : windows) {
-    WINDOW* win = win2.win;
-    wrefresh(win);
-    if(delay > 0ms) std::this_thread::sleep_for(delay);
-  }
-  refresh();
-}
-
 int main (int __attribute__((unused)) argc, char __attribute__((unused)) *argv[]) {
-
     mmask_t old = 0;
+    start_curses(old);
 
-    initscr();       // Initialize ncurses screen
-    cbreak();
-    noecho();        // Don't display user input
-    keypad(stdscr, TRUE);
-    // Enable mouse support and track all mouse events
-    // mousemask(BUTTON1_CLICKED | BUTTON2_CLICKED | BUTTON3_CLICKED | BUTTON4_PRESSED | BUTTON5_PRESSED | REPORT_MOUSE_POSITION | ALL_MOUSE_EVENTS, &old);
-    mousemask(BUTTON1_PRESSED | BUTTON1_RELEASED | BUTTON3_CLICKED |  REPORT_MOUSE_POSITION | ALL_MOUSE_EVENTS, &old);
-    // Set the mouse click interval to 1000 milliseconds (1 second)
-    mouseinterval(80);
+    // Thread for events
+    std::atomic<bool> mutex(false);
+    char cur_mon[50] = {0};
+    std::thread t(hyprSocketFocusMonitor, cur_mon, std::ref(mutex));
+    t.detach();
 
-    curs_set(0);     // Hide cursor
-
-
-    setlocale(LC_ALL, "");
     std::vector<WINDOW2> windows;
-
-    setcchar(&tl, L"╭", WA_NORMAL, 0, NULL);
-    setcchar(&bl, L"╰", WA_NORMAL, 0, NULL);
-    setcchar(&tr, L"╮", WA_NORMAL, 0, NULL);
-    setcchar(&br, L"╯", WA_NORMAL, 0, NULL);
-    setcchar(&s,  L"│", WA_NORMAL, 0, NULL);
-    setcchar(&h,  L"─", WA_NORMAL, 0, NULL);
-
-    if(!has_colors() || !can_change_color()){
-      printw("Terminal doesnt support colors or cannot change them!");
-      return -1;
-    }
-    start_color();
-    use_default_colors(); //Set colors to normal
-    init_pair(1, COLOR_WHITE, -1);
-    init_pair(2, COLOR_BLACK, -1);
-    init_pair(3, COLOR_YELLOW, -1);
-    init_pair(4, COLOR_BLUE, -1);
-    use_default_colors(); //Set colors to normal
 
     Json::Value root;
     if(!updateJson(root)){
       return 1;
     };
-
-    signal(SIGWINCH, [](int __attribute__((unused)) sig)->void {
-      endwin(); // End curses mode
-
-      refresh(); // Clear the screen
-      // Handle the resize event here
-      // ...
-      refresh(); // Redraw the screen
-      // Re-initialize ncurses
-      initscr();
-      // Turn off keyboard echoing and enable special key input
-      noecho();
-      keypad(stdscr, TRUE);
-    });
-
-    // Thread
-    std::atomic<bool> mutex(false);
-    char cur_mon[50] = {0};
-    std::thread t(hyprSocketFocusMonitor, cur_mon, std::ref(mutex));
-    t.detach();
 
     // Get Max Size
     int maxwidth = 0;
@@ -322,14 +103,8 @@ int main (int __attribute__((unused)) argc, char __attribute__((unused)) *argv[]
           getmaxyx(stdscr, row, col);
           updateWindowsFromJson(windows, root);
           renderWindows(windows);
-          /* mvprintw(1, 1, "status: %d", activeWin.mon.transform);
-          std::this_thread::sleep_for(249ms); */
           chg = false;
-        }
-        else if(activeWin != nullptr && ch == 'r') {
-          struct monitor &m = activeWin->mon;
-          transformMonitor(m.name, ++m.transform);
-          chg = true;
+          std::this_thread::sleep_for(250ms);
         }
         // mouse input
         else if (ch == KEY_MOUSE) {
@@ -337,23 +112,10 @@ int main (int __attribute__((unused)) argc, char __attribute__((unused)) *argv[]
           if (getmouse(&event) == OK) {
             WINDOW2* win2 = getWindowAt(windows, event.x, event.y);
             WINDOW* win = win2 ? win2->win : nullptr;
-            // if(event.bstate & REPORT_MOUSE_POSITION) {
-              /* nodelay(stdscr, FALSE);
-              ch = getch();
-              nodelay(stdscr, TRUE);
-              if(ch == 'r') {
-                if(win2 != nullptr) {
-                  refresh();
-                  transformMonitor(m.name, ++m.transform);
-                  wrefresh(win);
-                  refresh();
-                  chg = true;
-                  continue;
-                }
-              } */
-            // }
+            // mvprintw(1, 1, "Mouse event: %d at (%d,%d)", event.bstate, event.x, event.y);
+            /* if(event.bstate & REPORT_MOUSE_POSITION) {
+            } */
             if(event.bstate & BUTTON1_PRESSED){
-              // mvprintw(1, 1, "Mouse event: %d at (%d,%d)", event.bstate, event.x, event.y);
               if(win2 != nullptr){
                 refresh();
                 chg_border_col(3);
@@ -361,7 +123,7 @@ int main (int __attribute__((unused)) argc, char __attribute__((unused)) *argv[]
                 wrefresh(win);
                 refresh();
                 activeWin = win2;
-                chg = true;
+                // chg = true;
               }
             } else if(event.bstate & BUTTON1_RELEASED) {
               refresh();
@@ -376,13 +138,13 @@ int main (int __attribute__((unused)) argc, char __attribute__((unused)) *argv[]
                 wborder_set(xwin, (const cchar_t*)&s, (const cchar_t*)&s, (const cchar_t*)&h, (const cchar_t*)&h, (const cchar_t*)&tl, (const cchar_t*)&tr, (const cchar_t*)&bl, (const cchar_t*)&br);
                 wrefresh(xwin);
               }
-              // mvprintw(2, 1, "BUTTON1_RELEASED");
               chg_border_col(0);
               refresh();
-              if(win2){
+              if(win2) {
                 struct monitor &m = win2->mon;
+                // FIX: not rotatting if chg is set...
+                // chg = transformMonitor(m.name, ++m.transform);
                 transformMonitor(m.name, ++m.transform);
-                chg = true;
               }
               activeWin = nullptr;
             }
@@ -394,16 +156,16 @@ int main (int __attribute__((unused)) argc, char __attribute__((unused)) *argv[]
       }
     } while((ch = getch()) != 'q');
 
+// Clean up and exit
 error:
-    // Clean up and exit
     for(auto& win2 : windows){
       WINDOW* win = win2.win;
       delwin(win);
       win2.win = nullptr;
     }
     endwin();
+    // Stop detached thread
     mutex = true;
-    // hs_closeSocketConnection();
     std::this_thread::sleep_for(250ms);
     return 0;
 }
